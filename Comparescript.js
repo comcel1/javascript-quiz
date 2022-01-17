39d96f8484f20edc397f701468f933dc83ace991
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-01-16 20:06:21 -0600

    fixed js and css to match quiz styling

--- Comparescript.js
+++ Comparescript.js
@@ -1,3 +1,7 @@
+/** 
+ * Criteria 
+ */
+
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
@@ -9,121 +13,83 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
-// =======================================
 
+/** 
+ * DEFINE VARIABLES 
+ */
 
-// GIVEN I am taking a code quiz
-// Set of questions:
+ // Define a set of questions
 const questions = [
     {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+        answer: "d. <script>"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c. quotes"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "b. other arrays"
+    },
+    {
         question: "Commonly used data types DO NOT include:",
-        choices: ["a. strings", "booleans", "c. alerts", "d. numbers"],
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
         answer: "c. alerts"
     },
-    
     {
-        question: "______ JavaScript is also called client-side JavaScript.", 
-        choices: ["a. Microsoft", "b. Navigator", "c. LiveWire", "d. Native"],
-        answer: "b. Navigator"
+        question: "How do you create a function in JavaScript",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+        answer: "b. function myFunction()"
     },
-    
     {
-        question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
-        choices: ["a. The User's machine running a web browser", "b. The Web server", "c. A central machine deep within Netscape's corporate offices", "d. None of the above"],
-        answer: "a. The User's machine running a web browser"
+        question: "How do you call a function named myFunction?",
+        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+        answer: "c. myFunctions()"
     },
-    
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b. =="
+    },
     {
         question: "The first index of an array is ____.",
         choices: ["a. 0", "b. 1", "c. 8", "d. any"],
         answer: "a. 0"
     },
-        
     {
-        question: "______ JavaScript is also called client-side JavaScript.",
-        choices: ["a. Microsoft", "b. Navigator", "c. LiveWire", "d. Native"],
-        answer: "b. Navigator"
+        question: "Who invented JavaScript?",
+        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
+        answer: "c. Brendan Eich"
     },
-    
     {
-        question: "__________ JavaScript is also called server-side JavaScript.",
-        choices: ["a. Microsoft", "b. Navigator", "c. LiveWire", "d. Native"],
-        answer: "c. LiveWire"
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
+        answer: "c. if(i == 5)"
     },
-            
     {
-        question: "What are variables used for in JavaScript Programs?",
-        choices: ["a. Storing numbers, dates, or other values", "b. Varying randomly", "c. Causing high-school algebra flashbacks", "d. None of the above"],
-        answer: "a. Storing numbers, dates, or other values"
+        question: "How do you add a comment in a JavaScript?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        answer: "a. //This is a comment"
     },
-            
-    {        
-        question: "_____ JavaScript statements embedded in an HTML page can respond to user events such as mouse-clicks, form input, and page navigation.",
-        choices: ["a. Client-side", "b Server-side", "c. Local", "d. Native"],
-        answer: "a. Client-side"
-    },       
-    
     {
-        question: "Which of the following can't be done with client-side JavaScript?",
-        choices: ["a. Validating a form", "b. sending a form's contents by email", "c. storing the form's contents to a database file on the server", "d. None of the above"],
-        answer: "c. storing the form's contents to a database file on the server"
-     },  
-    
-    {
-        question: "Which of the following are capabilities of functions in JavaScript?", 
-        choices: ["a. Return a value", "b. Accept parameters and Return a value", "c. Accept parameters", "d. None of the above"],
-        answer: "c. Accept parameters"
-    },
-    
-    {
-        question: "Which of the following is not a valid JavaScript variable name?",
-        choices: ["a. 2names", "b. _first_and_last_names", "c. FirstAndLast", "d. None of the above"], 
-        answer: "a. 2names"
-    },
-    
-    {
-        question: "______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
-        choices: ["a. <SCRIPT>", "b. <BODY>", "c. <HEAD>", "d. <TITLE>",],
-        answer: "a. <SCRIPT>"
-    },
-    
-    {   
-        question: "How does JavaScript store dates in a date object?", 
-        choices: ["a. The number of milliseconds since January 1st, 1970", "b. The number of days since January 1st, 1900", "c. The number of seconds since Netscape's public stock offering.", "d. None of the above"],
-        answer: "a. The number of milliseconds since January 1st, 1970"
-    },      
-    
-    {
-        question: "Which of the following attribute can hold the JavaScript version?", 
-        choices: ["a. LANGUAGE", "b. SCRIPT", "c. VERSION", "d. None of the above"],
-        answer: "a. LANGUAGE"
-    },      
-            
-    {   
-        question: "What is the correct JavaScript syntax to write 'Hello World'?",
-        choices: ["a. System.out.println('Hello World')", "b. println ('Hello World')", "c. document.write('Hello World')", "d. response.write('Hello World')"],
-        answer: "c. document.write('Hello World')"
-    },
-    
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["a. <js>", "b. <scripting>", "c. <script>", "d. <javascript>"],
-        answer: "c. <script>"
-    }, 
-]
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a. onclick"
+    }
+];
 
-// VARIABLES
-
-// timer variables
+// grab references to elements
 var timer = document.getElementById("timer");
 var timeLeft = document.getElementById("timeLeft");
 var timesUp = document.getElementById("timesUp");
 
-// start quiz variables
 var startDiv = document.getElementById("start");
 var startQuizBtn = document.getElementById("start-quiz-button");
 
-// question and answer variables
 var questionDiv = document.getElementById("questionDiv");
 var questionTitle = document.getElementById("questionTitle");
 var choiceA = document.getElementById("btn0");
@@ -132,7 +98,6 @@ var choiceC = document.getElementById("btn2");
 var choiceD = document.getElementById("btn3");
 var answerCheck = document.getElementById("answerCheck");
 
-// score variables
 var summary = document.getElementById("summary");
 var submitInitialBtn = document.getElementById("submitInitialBtn");
 var initialInput = document.getElementById("initialInput");
@@ -152,13 +117,15 @@ var questionNum = 0;
 var scoreResult;
 var questionIndex = 0;
 
+/**
+ * FUNCTIONS
+ */
 
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-var totalTime = 60;
+// WHEN I click the start button, timer starts
+var totalTime = 151;
 function newQuiz() {
     questionIndex = 0;
-    totalTime = 60;
+    totalTime = 150;
     timeLeft.textContent = totalTime;
     initialInput.textContent = "";
 
@@ -176,13 +143,15 @@ function newQuiz() {
                 gameOver();
             }
         }
-    }, 1000);
+    },1000);
 
     showQuiz();
 };
 
+// console.log(questions[questionIndex].question);
+// console.log(questions[questionIndex].choices);
 
-// quiz start functions
+// then presented with questions and choices
 function showQuiz() {
     nextQuestion();
 }
@@ -203,11 +172,12 @@ function checkAnswer(answer) {
     answerCheck.style.display = "block";
 
     if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
-        // if answer is correct, add one point to score
+        // correct answer, add 1 score to final score
         correctAns++;
+        // console.log(correctAns);
         answerCheck.textContent = "Correct!";
     } else {
-        // if answer is wrong, deduct 10 seconds from timer
+        // wrong answer, deduct 10 second from timer
         totalTime -= 10;
         timeLeft.textContent = totalTime;
         answerCheck.textContent = "Wrong! The correct answer is: " + questions[questionIndex].answer;
@@ -218,17 +188,20 @@ function checkAnswer(answer) {
     if (questionIndex < questions.length) {
         nextQuestion();
     } else {
-        // if all questions have been answered or time runs out, game over
+        // if no more question, run game over function
         gameOver();
     }
 }
 
 function chooseA() { checkAnswer(0); }
+
 function chooseB() { checkAnswer(1); }
+
 function chooseC() { checkAnswer(2); }
+
 function chooseD() { checkAnswer(3); }
 
-// Game Over function
+// when all questions are answered or timer reaches 0, game over
 function gameOver() {
     summary.style.display = "block";
     questionDiv.style.display = "none";
@@ -244,7 +217,7 @@ function gameOver() {
 function storeHighScores(event) {
     event.preventDefault();
 
-    // If no initials, alert user
+    // stop function is initial is blank
     if (initialInput.value === "") {
         alert("Please enter your initials!");
         return;
@@ -274,11 +247,11 @@ function storeHighScores(event) {
     console.log(userScore);
     scoresArray.push(userScore);
 
-    // Convert to text
+    // stringify array in order to store in local
     var scoresArrayString = JSON.stringify(scoresArray);
     window.localStorage.setItem("high scores", scoresArrayString);
     
-    // Show high scores stored in local storage
+    // show current highscores
     showHighScores();
 }
 
@@ -295,7 +268,7 @@ function showHighScores() {
 
     var savedHighScores = localStorage.getItem("high scores");
 
-    // check if there are any high scoresGot in local storage
+    // check if there is any in local storage
     if (savedHighScores === null) {
         return;
     }
@@ -310,9 +283,10 @@ function showHighScores() {
     }
 }
 
-// EVENT LISTENERS
+/**
+ * ADD EVENT LISTENERS
+ */
 
-// start quiz and answer listeners
 startQuizBtn.addEventListener("click", newQuiz);
 choiceA.addEventListener("click", chooseA);
 choiceB.addEventListener("click", chooseB);
@@ -335,5 +309,5 @@ goBackBtn.addEventListener("click", function() {
 clearHighScoreBtn.addEventListener("click", function(){
     window.localStorage.removeItem("high scores");
     listOfHighScores.innerHTML = "High Scores Cleared!";
-    listOfHighScores.setAttribute("style")
+    listOfHighScores.setAttribute("style", "font-family: 'Archivo', sans-serif; font-style: italic;")
 });
